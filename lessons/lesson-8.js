4a18cd1d9808d18d892e5da70dd0e6a51d39ea23
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2026-01-08 00:48:56 -0500

    Add lesson 8

--- lessons/lesson-8.js
+++ lessons/lesson-8.js
@@ -35,3 +35,11 @@ function multiplyByTwo(number) {
 
 var myResult = multiplyByTwo(5)
 console.log(myResult)
+
+// import function
+import{ printAge } from '../helpers/printHelper.js'
+printAge(5)
+
+// import everything
+import * as helper from '../helpers/printHelper.js'
+helper.printAge(10)
