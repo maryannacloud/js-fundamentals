b4bba9e5d390bc17246fc34f15c46653854870df
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2026-01-07 21:39:46 -0500

    Add lesson 1

---
+++ lessons/lesson-1.js
@@ -0,0 +1,32 @@
+//1. Hello World
+console.log("Hello World")
+
+// Variables - entities that can save different data types for code execution
+var firstName = "John"
+let lastName = "Smith"
+console.log(firstName)
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "35"
+sex = "male"
+dateOfBirth = "08/18/1990"
+console.log(age)
+console.log(sex)
+console.log(dateOfBirth)
+age = "6"
+console.log(age)
+
+// Constants - can save the info similarly to variables, 
+// but the difference is that the value can be assigned / saved only once
+const occupation = "Enginner"
+console.log(occupation)
+
+// Data Types (5 primitive data types)
+var middleName = "David" // string
+var ageOfBrother = 25 // number
+var isHeMarried = false // boolean
+var yearsInMarriage = null // null = no value
+var numberOfCars = undefined // undefined
+
+
